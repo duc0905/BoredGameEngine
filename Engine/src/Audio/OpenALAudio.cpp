0fa1a3b8817027237d1569f6bb79df1f82cc9d5c
Giang Bui <bhuonggiang03@gmail.com>
2022-08-28 15:40:12 -0400

    restructure audio system files

---
+++ Engine/src/Audio/OpenALAudio.cpp
@@ -0,0 +1,8 @@
+//Create a new abstract class IAudio to specify the virtual methods its subclasses needs to implement
+//Create OpenALAudio class which implements the IAudio; then overrides methods
+//Add methods to setand get audio system in IGame
+//Create AudioComponent implementing Component.Add a method to play sound, a method to stop sound, and set sound
+
+//Does not need to abstract the concepts(Object, Source, Buffer) into classes yet.
+
+#include "pch.h"
