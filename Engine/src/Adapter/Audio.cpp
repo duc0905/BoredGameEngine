1e6ed187850e09b262e152319941af1cb420706b
Minh Pham <68952701+minhpham160603@users.noreply.github.com>
2023-07-03 23:58:01 +0100

    Add audio

--- Engine/src/Adapter/Audio.cpp
+++ Engine/src/Adapter/Audio.cpp
@@ -17,7 +17,7 @@ namespace Bored
 			alBufferData(id, getFormat(format), data, size, freq);
 		}
 
-		void OALBuffer::Delete()
+		OALBuffer::~OALBuffer()
 		{
 			alDeleteBuffers(1, &id);
 		}
@@ -39,5 +39,83 @@ namespace Bored
 			}
 		}
 
+		int OALBuffer::GetFreq(){
+			int value;
+			alGetBufferi(id, AL_FREQUENCY, &value);
+			return value;
+		}
+
+		int OALBuffer::GetBits(){
+			int value;
+			alGetBufferi(id, AL_BITS, &value);
+			return value;
+		}
+		int OALBuffer::GetChannels(){
+			int value;
+			alGetBufferi(id, AL_CHANNELS, &value);
+			return value;
+		}
+		int OALBuffer::GetSize(){
+			int value;
+			alGetBufferi(id, AL_SIZE, &value);
+			return value;
+		}
+		std::vector<int> OALBuffer::GetData(){
+			throw std::runtime_error("Get data not supported");
+		}
+
+		void OALSource::SetPosition(const glm::vec3& vec) {
+			alSource3f(source, AL_POSITION, vec[0], vec[1], vec[2]);
+		}
+		void OALSource::SetVelocit(const glm::vec3& vec) {
+			alSource3f(source, AL_VELOCITY, vec[0], vec[1], vec[2]);
+		}
+		void OALSource::SetDirection(const glm::vec3& vec) {
+			alSource3f(source, AL_DIRECTION, vec[0], vec[1], vec[2]);
+		}
+
+		void OALSource::SetPitch(float value) {
+			alSourcef(source, AL_PITCH, value);
+		}
+		void OALSource::SetGain(float value) {
+			alSourcef(source, AL_GAIN, value);
+		}
+		void OALSource::SetMinGain(float value) {
+			alSourcef(source, AL_MIN_GAIN, value);
+		}
+		void OALSource::SetMaxGain(float value) {
+			alSourcef(source, AL_MAX_GAIN, value);
+		}
+		void OALSource::SetMaxDistance(float value) {
+			alSourcef(source, AL_MAX_DISTANCE, value);
+		}
+		void OALSource::SetRolloffFactor(float value) {
+			alSourcef(source, AL_ROLLOFF_FACTOR, value);
+		}
+		void OALSource::SetConeOuterGain(float value) {
+			alSourcef(source, AL_CONE_OUTER_GAIN, value);
+		}
+		void OALSource::SetConeInnerAngle(float value) {
+			alSourcef(source, AL_CONE_INNER_ANGLE, value);
+		}
+		void OALSource::SetConeOuterAngle(float value) {
+			alSourcef(source, AL_CONE_OUTER_ANGLE, value);
+		}
+		void OALSource::SetReferenceDistance(float value) {
+			alSourcef(source, AL_REFERENCE_DISTANCE, value);
+		}
+
+		void OALSource::SetSourceRelative(unsigned int value) {
+			alSourcei(source, AL_SOURCE_RELATIVE, value);
+		};
+		void OALSource::SetLooping(unsigned int value) {
+			alSourcei(source, AL_LOOPING, value);
+		};
+		void OALSource::SetBuffer(unsigned int value) {
+			alSourcei(source, AL_BUFFER, value);
+		};
+		void OALSource::SetSourceState(unsigned int value) {
+			alSourcei(source, AL_SOURCE_STATE, value);
+		};
 	} // namespace Audio
 } // namespace Bored
--- Engine/src/Adapter/Audio.h
+++ Engine/src/Adapter/Audio.h
@@ -1,8 +1,10 @@
 #pragma once
-#include <memory>
 #include <string>
+#include <memory>
+#include <vector>
 #include <AL/alc.h>
 #include <AL/al.h>
+#include <glm/glm.hpp>
 
 namespace Bored
 {
@@ -20,12 +22,17 @@ namespace Bored
         {
         public:
             Buffer() = default;
-            ~Buffer() = default;
             Buffer(const void *data, size_t size, Format format, size_t freq);
+            virtual ~Buffer() {};
 
+            virtual bool IsValid() = 0;
             virtual void BufferData(const void *data, size_t size, Format format, size_t freq) = 0;
-            virtual void Delete() = 0;
 
+            virtual int GetFreq() = 0;
+            virtual int GetBits() = 0;
+            virtual int GetChannels() = 0;
+            virtual int GetSize() = 0;
+            virtual std::vector<int> GetData() = 0;
         private:
             size_t size, freq;
             Format format;
@@ -35,33 +42,94 @@ namespace Bored
         {
         public:
             OALBuffer() = default;
-            ~OALBuffer() = default;
+            virtual ~OALBuffer() override;
             OALBuffer(const void *data, size_t size, Format format, size_t freq);
-            virtual void BufferData(const void *data, size_t size, Format format, size_t freq) override;
-            virtual void Delete() override;
 
+            virtual int GetFreq() override;
+            virtual int GetBits() override;
+            virtual int GetChannels() override;
+            virtual int GetSize() override;
+            virtual std::vector<int> GetData() override;
+            virtual bool IsValid() override;
+            virtual void BufferData(const void *data, size_t size, Format format, size_t freq) override;
         private:
             ALenum getFormat(Format format);
-
             ALuint id;
-        };
-
-        class Listener
-        {
         };
 
         class Source
         {
+        public:
+            virtual ~Source() {};
+            virtual bool IsValid() = 0;
+
+            virtual void SetPosition(const glm::vec3&) = 0;
+            virtual void SetVelocit(const glm::vec3&) = 0;
+            virtual void SetDirection(const glm::vec3&) = 0;
+
+            virtual void SetPitch(float) = 0;
+            virtual void SetGain(float) = 0;
+            virtual void SetMinGain(float) = 0;
+            virtual void SetMaxGain(float) = 0;
+            virtual void SetMaxDistance(float) = 0;
+            virtual void SetRolloffFactor(float) = 0;
+            virtual void SetConeOuterGain(float) = 0;
+            virtual void SetConeInnerAngle(float) = 0;
+            virtual void SetConeOuterAngle(float) = 0;
+            virtual void SetReferenceDistance(float) = 0;
+
+            virtual void SetSourceRelative(unsigned int) = 0;
+            virtual void SetLooping(unsigned int) = 0;
+            virtual void SetBuffer(unsigned int) = 0;
+            virtual void SetSourceState(unsigned int) = 0;
+        };
+
+        class OALSource : public Source {
+        public:
+            OALSource();
+            virtual ~OALSource() override;
+
+            virtual bool IsValid() override;
+
+            virtual void SetPosition(const glm::vec3&) override;
+            virtual void SetVelocit(const glm::vec3&) override;
+            virtual void SetDirection(const glm::vec3&) override;
+
+            virtual void SetPitch(float) override;
+            virtual void SetGain(float) override;
+            virtual void SetMinGain(float) override;
+            virtual void SetMaxGain(float) override;
+            virtual void SetMaxDistance(float) override;
+            virtual void SetRolloffFactor(float) override;
+            virtual void SetConeOuterGain(float) override;
+            virtual void SetConeInnerAngle(float) override;
+            virtual void SetConeOuterAngle(float) override;
+            virtual void SetReferenceDistance(float) override;
+
+            virtual void SetSourceRelative(unsigned int) override;
+            virtual void SetLooping(unsigned int) override;
+            virtual void SetBuffer(unsigned int) override;
+            virtual void SetSourceState(unsigned int) override;
+        private:
+            ALuint source;
+            // @PTM thich de j vao day thi de
+        };
+
+        class Listener
+        {
+        public:
+            virtual ~Listener() {};
+        };
+
+        class OALListener : public Listener {
+        public:
+            virtual ~OALListener() override;
         };
 
         class Context
         {
+        public:
+            virtual ~Context() {};
         };
-
-        std::shared_ptr<Buffer> createBuffer(const void *data)
-        {
-            return std::make_shared<OALBuffer>();
-        }
-
     }
 }
