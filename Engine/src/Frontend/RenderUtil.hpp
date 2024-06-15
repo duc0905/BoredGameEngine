aa1a555eadc6f6a8aa82338521dec55429c17743
Duc Le <minhduc.le@mail.utoronto.ca>
2024-06-15 01:05:46 -0400

    Clean up files in Engine

--- Engine/src/Frontend/RenderUtil.hpp
+++ Engine/src/Frontend/RenderUtil.hpp
@@ -7,13 +7,6 @@ namespace Bored
 {
 namespace Render
 {
-// struct Texture
-// {
-//     std::string name;
-//     unsigned int width, height, bpp;
-//     unsigned char* data;
-// };
-
 struct Material
 {
     std::string name;
@@ -47,54 +40,3 @@ struct Model
 };
 }
 }
-
-// class TextureFactory
-// {
-// private:
-//     static std::shared_ptr<Texture> Find(const std::string& path);
-//     TextureFactory() = delete;
-//     ~TextureFactory() = delete;
-//
-// public:
-//     static std::vector<std::shared_ptr<Texture>> storage;
-//     static std::shared_ptr<Texture> Load(const std::string& path);
-//     static std::shared_ptr<Texture> Load(const std::string& name, unsigned int w, unsigned int h, unsigned int bpp, unsigned char* data);
-//     static std::shared_ptr<Texture> Load(const Texture& tex);
-// };
-//
-// class MaterialFactory
-// {
-//     static std::vector<std::shared_ptr<Material>> storage;
-//     static std::shared_ptr<Material> Find(const std::string& path);
-//     // MaterialFactory() = delete;
-// public:
-//     static std::shared_ptr<Material> Load(const std::string& name, float shininess, float reflectiveness,
-//                                           std::shared_ptr<Texture> diff = nullptr,
-//                                           std::shared_ptr<Texture> spec = nullptr);
-//     static std::shared_ptr<Material> Load(const Material& mat);
-//     static std::shared_ptr<Material> Load(const std::string& name);
-//     MaterialFactory() = delete;
-//     ~MaterialFactory() = delete;
-// };
-//
-// class MeshFactory
-// {
-//     static std::vector<std::shared_ptr<Mesh>> storage;
-//     static std::shared_ptr<Mesh> Find(const std::string& name);
-//     MeshFactory() = delete;
-//
-// public:
-//     static std::shared_ptr<Mesh> Load(const std::string& name);
-//     static std::shared_ptr<Mesh> Load(const std::string& name, std::vector<glm::vec3>, std::vector<glm::vec2>,
-//                                       std::vector<glm::vec3>, std::vector<unsigned int>);
-//     static std::shared_ptr<Mesh> Load(const Mesh& mesh);
-//     // MeshFactory() = delete;
-//     ~MeshFactory() = delete;
-// };
-// }
-//
-// namespace Helper
-// {
-// Render::Model Load(const std::string& file);
-// }
-// } // namespace Bored
