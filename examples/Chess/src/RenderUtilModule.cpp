c44cfa67b942a1fc133f1aebf748dbd58185ece8
Duc Le <minhduc.le@mail.utoronto.ca>
2024-08-14 00:00:35 -0400

    Create boilerplate code for the render utility conversion module
    - TODO: Implement the module to convert into OGL utilities

---
+++ examples/Chess/src/RenderUtilModule.cpp
@@ -0,0 +1,28 @@
+#include "RenderUtilModule.hpp"
+
+// TODO: Implement
+
+RenderUtilModule::RenderUtilModule()
+{
+}
+
+RenderUtilModule::~RenderUtilModule()
+{
+}
+
+void RenderUtilModule::OnSetup()
+{
+}
+
+void RenderUtilModule::OnSwitchScene()
+{
+}
+
+bool RenderUtilModule::OnUpdate(double dt)
+{
+  return true;
+}
+
+void RenderUtilModule::OnShutdown()
+{
+}
