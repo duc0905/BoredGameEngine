824197d65b401b305c2898c0ceef4b0c175b7384
Duc Le <minhduc.le@mail.utoronto.ca>
2024-08-20 00:40:45 -0400

    Setup tests for Engine
    - Add google test to Engine
    - Write dummy tests for mesh, factory, and texture

---
+++ Engine/src/Frontend/CPU/Factory.spec.cpp
@@ -0,0 +1,6 @@
+#include <gtest/gtest.h>
+
+TEST(FactorySpec, Example)
+{
+    EXPECT_EQ(2 + 3, 5);
+}
---
+++ Engine/src/Frontend/CPU/Mesh.spec.cpp
@@ -0,0 +1,6 @@
+#include <gtest/gtest.h>
+
+TEST(MeshSpec, Example)
+{
+    EXPECT_EQ(2 + 3, 5);
+}
