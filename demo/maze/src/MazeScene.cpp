6064872f6f89fbfdf8bf048019d262d889c5f846
Duc Le <minhduc.le@mail.utoronto.ca>
2025-09-16 15:12:36 -0400

    Create Maze runner demo project
    - Create CMake target
    - Add asset packs: kaykit_prototype and kenney_minimap

---
+++ demo/maze/src/MazeScene.cpp
@@ -0,0 +1,139 @@
+#include "MazeScene.hpp"
+#include "Components/Camera.hpp"
+#include "Components/Lighting.hpp"
+#include "Components/Mesh3D.hpp"
+#include "Components/MeshComponent.hpp"
+#include "Systems/Input/InputSystem.hpp"
+#include "Systems/Renderer/OGL/Renderer.hpp"
+#include <GLFW/glfw3.h>
+
+void PlayerController::OnInput(double dt, Bored::InputEvent &event,
+                               std::shared_ptr<Bored::Node> node) {
+  if (event.type == Bored::InputType::KEY_DOWN ||
+      event.type == Bored::InputType::KEY_REPEAT) {
+    // auto movement_comp = node->GetComponent<MovementComponent>();
+    glm::vec3 direction(0.0f);
+
+    if (event.key.keyCode == GLFW_KEY_W) {
+      direction = {0.0f, 0.0f, -1.0f};
+      event.handled = true;
+    }
+
+    if (event.key.keyCode == GLFW_KEY_A) {
+      direction = {-1.0f, 0.0f, 0.0f};
+      event.handled = true;
+    }
+
+    if (event.key.keyCode == GLFW_KEY_S) {
+      direction = {0.0f, 0.0f, 1.0f};
+      event.handled = true;
+    }
+
+    if (event.key.keyCode == GLFW_KEY_D) {
+      direction = {1.0f, 0.0f, 0.0f};
+      event.handled = true;
+    }
+
+    if (event.key.keyCode == GLFW_KEY_SPACE) {
+      direction = {0.0f, 1.0f, 0.0f};
+      event.handled = true;
+    }
+
+    if (event.key.keyCode == GLFW_KEY_LEFT_CONTROL) {
+      direction = {0.0f, -1.0f, 0.0f};
+      event.handled = true;
+    }
+
+    if (event.handled) {
+      direction =
+          node->transform.GetTransformMatrix() * glm::vec4(direction, 0.0f);
+
+      direction = glm::normalize(direction);
+
+      node->transform.translate += direction * speed * (float)dt;
+    }
+  }
+
+  if (is_mouse_hidden) {
+    if (event.type == Bored::InputType::MOUSE_MOVE) {
+      glm::vec3 rotation{-event.mouseMove.dy, -event.mouseMove.dx, 0.0f};
+      rotation *= dt * sensivity;
+      node->transform.rotate += rotation;
+      event.handled = true;
+    }
+  }
+
+  if (event.type == Bored::InputType::KEY_DOWN &&
+      event.key.keyCode == GLFW_KEY_ESCAPE) {
+    if (is_mouse_hidden) {
+      node->scene.context.io->SetCursorMode(Bored::CursorMode::VISIBLE);
+      is_mouse_hidden = false;
+    } else {
+      node->scene.context.io->SetCursorMode(Bored::CursorMode::DISABLED);
+      is_mouse_hidden = true;
+    }
+  }
+}
+
+MazeScene::MazeScene() {
+  // Setup window system
+  io = std::make_shared<Bored::IOService>(SCR_WIDTH, SCR_HEIGHT);
+  renderer = std::make_shared<OGL::Renderer>(*io);
+  systems.push_back(renderer);
+
+  // Setup input system
+  std::shared_ptr<Bored::Input> input = std::make_shared<Bored::Input>(*io);
+  systems.push_back(input);
+
+  // Populate the services into scene context
+  context.io = io.get();
+}
+
+void MazeScene::BuildScene() {
+  // Building scene
+  std::shared_ptr<Bored::Node> root = CreateNode();
+  SetRoot(root);
+
+  // Player node
+  std::shared_ptr<Bored::Node> player_node = CreateNode();
+  player_node->AddComponent<Bored::CameraComponent>(new Bored::Perspective(
+      Bored::Perspective::GetFOV(1.0f, 2.0f), SCR_WIDTH, SCR_HEIGHT, 1.0f));
+  // Player control
+  Bored::InputComponent &player_input =
+      player_node->AddComponent<Bored::InputComponent>();
+  player_input.input_handler = std::make_shared<PlayerController>();
+  player_node->transform.translate = {3.0f, 2.0f, 4.0f};
+  SetActiveCamera(player_node);
+  root->AddChild(player_node);
+
+  // Walls and floors
+  // Loading models
+  // TODO: should let the asset registry handle this
+  std::shared_ptr<Bored::ArrayMesh> floor_tile_model = OGL::LoadModel(
+      resource_path + "models/kaykit_prototype/Primitive_Floor.gltf");
+  std::shared_ptr<Bored::Material> black_tile_mat =
+      std::make_shared<Bored::Material>();
+
+  // Massive 10x10 floor
+  for (int i = 0; i < 10; i++) {
+    for (int j = 0; j < 10; j++) {
+      std::shared_ptr<Bored::Node> floor_tile = CreateNode();
+      auto &mesh_comp = floor_tile->AddComponent<Bored::MeshComponent>();
+      mesh_comp.mesh = floor_tile_model;
+      mesh_comp.mesh->material;
+      floor_tile->transform.translate = {i * 1.0f, 0.0f, j * 1.0f};
+      floor_tile->transform.scale = {0.25, 0.5f, 0.25f};
+      root->AddChild(floor_tile);
+    }
+  }
+
+  // Lighting
+  std::shared_ptr<Bored::Node> dir_light_node = CreateNode();
+  Bored::DirectionalLight &dir_light_comp =
+      dir_light_node->AddComponent<Bored::DirectionalLight>();
+  dir_light_comp.light_color = {0.8f, 0.6f, 0.2f};
+  dir_light_node->transform.rotate = {0.3f, 0.1f, 0.0f};
+  root->AddChild(dir_light_node);
+}
+
+void MazeScene::AfterBuildScene() { renderer->SetupObjects(*this); }
---
+++ demo/maze/src/MazeScene.hpp
@@ -0,0 +1,34 @@
+#pragma once
+
+#include "Components/InputComponent.hpp"
+#include "Scene/Scene.hpp"
+#include "Systems/Renderer/OGL/Renderer.hpp"
+
+// settings
+const unsigned int SCR_WIDTH = 1200;
+const unsigned int SCR_HEIGHT = 800;
+const std::string resource_path = "demo/maze/res/";
+
+class PlayerController : public Bored::InputHandler {
+public:
+  virtual void OnInput(double dt, Bored::InputEvent &event,
+                       std::shared_ptr<Bored::Node> node) override;
+
+private:
+  float speed = 10.0f;
+  float sensivity = 0.8f;
+  bool is_mouse_hidden = false;
+};
+
+class MazeScene : public Bored::Scene {
+public:
+  MazeScene();
+
+  virtual void BuildScene() override;
+
+  virtual void AfterBuildScene() override;
+
+private:
+  std::shared_ptr<OGL::Renderer> renderer;
+  std::shared_ptr<Bored::IOService> io;
+};
